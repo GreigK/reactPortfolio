915956e190ef09b286b0ca3a3f43ded69ac47bde
Greig Kerr <greigkerr@virginmedia.com>
2022-08-29 02:07:23 +0100

    website design made with some CSS and placeholder

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <h1><b>About me!</b></h1>
+      <h2>Greig Kerr</h2>
+      <p>
+        I am a full stack software developer in training, taking part in a 16 week intensive Software Developent Course 
+        through <a href='https://codeclan.com/'>CodeClan</a>.
+        With previous experieice as a Customer Service Consultant with HMRC, I am changing 
+        my career plans as i've found a love exploring new technologies!
+      </p>
+
+    </div>
+  )
+}
+
+export default About
---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,25 @@
+import proj1 from '../images/proj1.png';
+import proj2 from '../images/proj2.jpeg';
+import proj3 from '../images/proj3.jpeg';
+
+export const projectList = [
+    {
+        name: "Gym Booking App",
+        image: proj1,
+        technologies: "HTML / CSS Python Flask PostgreSQL and the psycopg",
+        link: "https://github.com/GreigK/Python-Project-Gym"
+    },
+    {
+        name: "Placeholder Javascript Project",
+        image: proj2,
+        technologies: "PlaceHolder ",
+        link: "https://github.com/GreigK/Python-Project-Gym"
+    },
+    {
+        name: "Placeholder Final Project",
+        image: proj3,
+        technologies: "PlaceHolder ",
+        link: "https://github.com/GreigK/Python-Project-Gym"
+    },
+];
+
